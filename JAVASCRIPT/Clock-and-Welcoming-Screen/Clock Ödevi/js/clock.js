f2da677504e886f7913bd227bfab78e2fb292804
mertoztat <mertoztat@gmail.com>
2022-01-16 16:12:44 +0300

    new changes

--- JAVASCRIPT/Clock-and-Welcoming-Screen/Clock Ödevi/js/clock.js
+++ JAVASCRIPT/Clock-and-Welcoming-Screen/Clock Ödevi/js/clock.js
@@ -1,10 +1,9 @@
 let userName = document.getElementById("myName")
 let clock = document.getElementById("myClock")
-
 let weekDay = ["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"]
 
 userName = prompt("Adınız nedir?")
-myName.innerHTML = `Hoşgeldin ${userName}`
+
 
 function showTime(){
     const today = new Date()
@@ -13,9 +12,12 @@ function showTime(){
     let s = today.getSeconds()
     let d = weekDay[today.getDay()] 
 
-    clock.innerHTML = h + ":" + m + ":" + s + "" + d
+
+    myName.innerHTML = `Hoşgeldin ${userName}`
+    clock.innerHTML = h + ":" + m + ":" + s + " " + d
 
 setTimeout(showTime, 1000)    
 }
+
 showTime()
 
